d9f9aca2bd055e7d4a827d557fda87e4f9d284bd
rdnt42 <radiant42@yandex.ru>
2022-06-22 00:30:20 +0300

    добавлен запрос с выводом времени

--- src/main/java/com/example/javainterview/ControllerPerson.java
+++ src/main/java/com/example/javainterview/ControllerPerson.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA.
  * User: marowak
@@ -20,5 +22,16 @@ public class ControllerPerson {
     @RequestMapping(value = "/api/v1/person", method = RequestMethod.POST)
     public Person getPerson(@RequestBody PersonRequest personRequest) {
         return repositoryPerson.getById(Long.parseLong(personRequest.id));
+    }
+
+
+    @RequestMapping(value = "/api/v1/personAnLogTime", method = RequestMethod.POST)
+    public Person getPersonAnLogTime(@RequestBody PersonRequest personRequest) {
+        long start = new Date().getTime();
+        Person person = repositoryPerson.getById(Long.parseLong(personRequest.id));
+        long stop = new Date().getTime() - start;
+        System.out.println("Total time: " + (start - stop));
+
+        return person;
     }
 }
